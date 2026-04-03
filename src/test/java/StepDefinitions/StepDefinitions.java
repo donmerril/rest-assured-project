ee1759484d024394745fd35fcbbe508a6877268e
donmerril <donmerril@gmail.com>
2026-04-03 12:26:27 +0530

    Add implementation to Step Definitions class

--- src/test/java/StepDefinitions/StepDefinitions.java
+++ src/test/java/StepDefinitions/StepDefinitions.java
@@ -1,5 +1,31 @@
 package StepDefinitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefinitions {
 
+	
+	
+	@Given("Add Place Payload is ready")
+	public void add_place_payload_is_ready() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@When("User calls the {string}")
+	public void user_calls_the(String string) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("Response status code should be {int}")
+	public void response_status_code_should_be(Integer int1) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+	@Then("{string} in response body is {string}")
+	public void in_response_body_is(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
 }
